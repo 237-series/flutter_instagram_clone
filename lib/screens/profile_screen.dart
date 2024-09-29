122c30d2faade5037c6b424cf9121917bc0c7839
sglee237 <sglee237@ibs.re.kr>
2024-09-29 16:33:06 +0900

    profile update

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,24 +1,45 @@
 import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
-  // 임시로 9개의 포스트 이미지 데이터를 사용 (3x3 그리드)
-  final List<String> postImages = [
-    'assets/images/post_ex_01.jpg',
-    'assets/images/post_ex_02.jpg',
-    'assets/images/post_ex_03.jpg',
-    'assets/images/post_ex_01.jpg',
-    'assets/images/post_ex_02.jpg',
-    'assets/images/post_ex_03.jpg',
-    'assets/images/post_ex_01.jpg',
-    'assets/images/post_ex_02.jpg',
-    'assets/images/post_ex_03.jpg',
-  ];
+class ProfileScreen extends StatefulWidget {
+  final Map<String, dynamic>? profileData; // nullable로 유지
+
+  ProfileScreen({this.profileData});
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  int _selectedTab = 0; // 0: 포스트 모아보기, 1: 내가 태그된 사진
+  late Map<String, dynamic> profileData; // 프로필 데이터
+
+  final Map<String, dynamic> defaultProfileData = {
+    'profileImage': 'assets/images/profile1.jpg',
+    'username': 'user1',
+    'posts': 4,
+    'followers': 120,
+    'following': 50,
+    'postImages': [
+      'assets/images/post_ex_01.jpg',
+      'assets/images/post_ex_02.jpg',
+      'assets/images/post_ex_03.jpg',
+    ],
+    'taggedImages': [
+      'assets/images/tagged_ex_01.jpg',
+    ],
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    profileData = widget.profileData ?? defaultProfileData; // 프로필 데이터 설정
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('user1'),
+        title: Text(profileData['username']),
         actions: [_buildMenuButton()],
       ),
       body: SingleChildScrollView(
@@ -26,9 +47,9 @@ class ProfileScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             _buildProfileInfo(),
-            _buildTabBar(),
+            _buildTabBar(), // 탭바
             Divider(),
-            _buildPostGrid(),
+            _buildPostGrid(), // 탭에 따른 그리드 표시
           ],
         ),
       ),
@@ -41,14 +62,23 @@ class ProfileScreen extends StatelessWidget {
       padding: const EdgeInsets.all(16.0),
       child: Row(
         children: [
-          CircleAvatar(radius: 40, backgroundImage: AssetImage('assets/images/profile1.jpg')),
+          CircleAvatar(
+            radius: 40,
+            backgroundImage: AssetImage(profileData['profileImage']),
+          ),
           SizedBox(width: 16),
           Expanded(
             child: Column(
               children: [
                 _buildStatsRow(),
                 SizedBox(height: 12),
-                _buildActionButtons(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    _buildActionButton('프로필 편집'),
+                    _buildActionButton('프로필 공유'),
+                  ],
+                ),
               ],
             ),
           ),
@@ -62,9 +92,9 @@ class ProfileScreen extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        _buildStatColumn('4', '게시물'),
-        _buildStatColumn('0', '팔로워'),
-        _buildStatColumn('1', '팔로잉'),
+        _buildStatColumn(profileData['posts'].toString(), '게시물'),
+        _buildStatColumn(profileData['followers'].toString(), '팔로워'),
+        _buildStatColumn(profileData['following'].toString(), '팔로잉'),
       ],
     );
   }
@@ -79,39 +109,33 @@ class ProfileScreen extends StatelessWidget {
     );
   }
 
-  // 프로필 편집, 프로필 공유 버튼
-  Widget _buildActionButtons() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildActionButton('프로필 편집'),
-        _buildActionButton('프로필 공유'),
-      ],
-    );
-  }
-
-  // 버튼을 생성하는 위젯
-  Widget _buildActionButton(String label) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: Text(label),
-      style: ElevatedButton.styleFrom(minimumSize: Size(160, 36)),
-    );
-  }
-
   // 간이 탭바 (포스트 모아보기, 내가 태그된 사진)
   Widget _buildTabBar() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        IconButton(icon: Icon(Icons.grid_on), onPressed: () {}),
-        IconButton(icon: Icon(Icons.person_pin), onPressed: () {}),
+        _buildTabIcon(Icons.grid_on, 0), // 포스트 모아보기
+        _buildTabIcon(Icons.person_pin, 1), // 내가 태그된 사진
       ],
     );
   }
 
-  // 포스트 미리보기 그리드
+  Widget _buildTabIcon(IconData icon, int index) {
+    return IconButton(
+      icon: Icon(icon),
+      color: _selectedTab == index ? Colors.blue : Colors.grey,
+      onPressed: () {
+        setState(() {
+          _selectedTab = index; // 선택한 탭 변경
+        });
+      },
+    );
+  }
+
+  // 포스트 미리보기 그리드 또는 내가 태그된 사진 그리드
   Widget _buildPostGrid() {
+    final images = _selectedTab == 0 ? profileData['postImages'] : profileData['taggedImages'];
+
     return GridView.builder(
       shrinkWrap: true,
       physics: NeverScrollableScrollPhysics(),
@@ -120,9 +144,9 @@ class ProfileScreen extends StatelessWidget {
         crossAxisSpacing: 2,
         mainAxisSpacing: 2,
       ),
-      itemCount: postImages.length,
+      itemCount: images.length,
       itemBuilder: (context, index) {
-        return Image.asset(postImages[index], fit: BoxFit.cover);
+        return Image.asset(images[index], fit: BoxFit.cover);
       },
     );
   }
@@ -132,6 +156,15 @@ class ProfileScreen extends StatelessWidget {
     return IconButton(
       icon: Icon(Icons.menu),
       onPressed: () {},
+    );
+  }
+
+  // 버튼을 생성하는 위젯
+  Widget _buildActionButton(String label) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text(label),
+      style: ElevatedButton.styleFrom(minimumSize: Size(160, 36)),
     );
   }
 }
