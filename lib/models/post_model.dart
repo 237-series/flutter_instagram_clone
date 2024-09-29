fd2049fa4c51ff5f39bb8e181262e50bf32ae764
sglee237 <sglee237@ibs.re.kr>
2024-09-29 20:54:39 +0900

    adding hive package, post_model

---
+++ lib/models/post_model.dart
@@ -0,0 +1,31 @@
+import 'package:hive/hive.dart';
+
+part 'post_model.g.dart'; // Hive generator가 생성한 파일
+
+@HiveType(typeId: 0) // HiveType 어노테이션 추가
+class Post {
+  @HiveField(0) // HiveField 어노테이션 추가
+  final String profileImage;
+  @HiveField(1) // HiveField 어노테이션 추가
+  final String username;
+  @HiveField(2) // HiveField 어노테이션 추가
+  final String postImage;
+  @HiveField(3) // HiveField 어노테이션 추가
+  final int likes;
+  @HiveField(4) // HiveField 어노테이션 추가
+  final int comments;
+  @HiveField(5) // HiveField 어노테이션 추가
+  final String description;
+  @HiveField(6) // HiveField 어노테이션 추가
+  final String date;
+
+  Post({
+    required this.profileImage,
+    required this.username,
+    required this.postImage,
+    required this.likes,
+    required this.comments,
+    required this.description,
+    required this.date,
+  });
+}
---
+++ lib/models/post_model.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class PostAdapter extends TypeAdapter<Post> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Post read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Post(
+      profileImage: fields[0] as String,
+      username: fields[1] as String,
+      postImage: fields[2] as String,
+      likes: fields[3] as int,
+      comments: fields[4] as int,
+      description: fields[5] as String,
+      date: fields[6] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Post obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.profileImage)
+      ..writeByte(1)
+      ..write(obj.username)
+      ..writeByte(2)
+      ..write(obj.postImage)
+      ..writeByte(3)
+      ..write(obj.likes)
+      ..writeByte(4)
+      ..write(obj.comments)
+      ..writeByte(5)
+      ..write(obj.description)
+      ..writeByte(6)
+      ..write(obj.date);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is PostAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
